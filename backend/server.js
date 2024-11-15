1694263ab307a868da0458bce5c8f5218f0da947
Noud63 <noudvandun@gmail.com>
2024-11-15 16:02:15 +0100

    cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config()
 
 app.use(
   cors({
-    origin: ["https://https://eredivisie.vercel.app"],
+    origin: ["http://localhost:3000", "https://https://eredivisie.vercel.app"],
     methods: ["GET"],
     credentials: true,
   })
