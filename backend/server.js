dfc28717fc12a4d5642e13c7d5abb26c16f8d4bb
Noud63 <noudvandun@gmail.com>
2024-11-18 08:50:08 +0100

    *

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use('/', express.static(path.join(__dirname, '../frontend/', 'build')))
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname, '../frontend/build'))
 });
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
