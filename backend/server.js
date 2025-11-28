cdfa761c9f8fee6361efdcd5d0cb3093bf717b1e
Noud63 <noudvandun@gmail.com>
2025-11-28 13:13:38 +0100

    stadions

--- backend/server.js
+++ backend/server.js
@@ -19,18 +19,18 @@ dotenv.config()
 //   })
 // );
 
-// app.get("/api/footballData", (req, res) => {
-//   res.json({ message: "Success"});
-// });
-
-
-app.use("/api/footballData", footballDataRoute);
-
-app.use('/', express.static(path.join(__dirname, '../frontend/', 'build')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+app.get("/api/footballData", (req, res) => {
+  res.json({ message: "Success"});
 });
+
+
+// app.use("/api/footballData", footballDataRoute);
+
+// app.use('/', express.static(path.join(__dirname, '../frontend/', 'build')))
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+// });
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
 
