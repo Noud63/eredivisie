89e564e6e05d6e3918456d6a568598de1a8574fe
Noud63 <noudvandun@gmail.com>
2025-12-11 14:51:52 +0100

    scrollToTop

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import goal from "../images/goal.jpg";
 import eredivisie65 from "../images/eredivisie65.png";
 import goal2 from "../images/goal2.jpg";
 import goal2_small from "../images/goal2_small.jpg";
 import goal3 from "../images/goal3.jpg";
-import goal3_small from "../images/goal3_small.jpg";    
+import goal3_small from "../images/goal3_small.jpg";
 import goal4 from "../images/goal4.jpg";
 import goal_small from "../images/goal_small.jpg";
 import goal4_small from "../images/goal4_small.jpg";
@@ -13,21 +14,36 @@ import Stand from "./Stand";
 import Recent from "./Recent";
 import styles from "../styles/Home.module.css";
 import Clubs from "./Clubs";
-import { Link } from "react-router-dom";
+import Image from "next/image";
+import Link from "next/link";
 import { useGlobalContext } from "./Context";
+import ScrollToTop from "./ScrollToTop";
 
 const Home = () => {
+
   const { topScorers, season } = useGlobalContext();
-  // console.log("Top:", topScorers);
 
   return (
     <div className={styles.home}>
       <div className={styles.spacer}></div>
       <div className={styles.schaal}>
-        <img src={goal3} className={styles.schaalPic} alt="" />
-        <img src={goal3_small} className={styles.schaalPic_small} alt="" />
+        <Image
+          src={goal3}
+          width={0}
+          height={0}
+          className={styles.schaalPic}
+          alt="goal"
+        />
+        <Image
+          src={goal3_small}
+          width={0}
+          height={0}
+          className={styles.schaalPic_small}
+          alt="schaal"
+        />
         <div className={styles.ere65}>
-          <img src={eredivisie65} alt="" className={styles.eredivisie65} />
+          <Image src={eredivisie65} width={0}
+          height={0} alt="" className={styles.eredivisie65} />
         </div>
         <div className={styles.history}>
           Het ontstaan van de Eredivisie gaat ver terug in de tijd. De voorloper
@@ -39,7 +55,7 @@ const Home = () => {
           Eredivisie officieel opgericht. Zodoende zijn sinds 1956 de sterkste
           clubs van Nederland verenigd in één landelijke competitie.
           <br />
-          <Link to="/history" style={{ textDecoration: "none" }}>
+          <Link href="/history" style={{ textDecoration: "none" }}>
             <span className={styles.leeshiermeer}>Lees hier meer.......</span>
           </Link>
         </div>
@@ -57,7 +73,7 @@ const Home = () => {
           Eredivisie officieel opgericht. Zodoende zijn sinds 1956 de sterkste
           clubs van Nederland verenigd in één landelijke competitie.
           <br />
-          <Link to="/history" style={{ textDecoration: "none" }}>
+          <Link href="/history" style={{ textDecoration: "none" }}>
             <span className={styles.leeshiermeer}>Lees hier meer.......</span>
           </Link>
         </div>
@@ -68,8 +84,10 @@ const Home = () => {
       </div>
 
       <div className={styles.borderMarker2}>
-        <img src={goal} className={styles.ballInGoal} alt="" />
-        <img src={goal_small} className={styles.ballInGoal_small} alt="" />
+        <Image src={goal} width={0}
+          height={0} className={styles.ballInGoal} alt="" />
+        <Image src={goal_small} width={0}
+          height={0} className={styles.ballInGoal_small} alt="" />
       </div>
 
       <div className={styles.stand}>
@@ -77,8 +95,10 @@ const Home = () => {
       </div>
 
       <div className={styles.borderMarker}>
-        <img src={goal4} className={styles.ballInGoal} alt="" />
-        <img src={goal4_small} className={styles.ballInGoal_small} alt="" />
+        <Image src={goal4} width={0}
+          height={0} className={styles.ballInGoal} alt="" />
+        <Image src={goal4_small} width={0}
+          height={0} className={styles.ballInGoal_small} alt="" />
       </div>
 
       <div className={styles.allmatches}>
@@ -86,30 +106,38 @@ const Home = () => {
       </div>
 
       <div className={styles.borderMarker}>
-        <img src={goal2} className={styles.ballInGoal} alt="" />
-        <img src={goal2_small} className={styles.ballInGoal_small} alt="" />
+        <Image src={goal2} width={0}
+          height={0} className={styles.ballInGoal} alt="" />
+        <Image src={goal2_small} width={0}
+          height={0} className={styles.ballInGoal_small} alt="" />
       </div>
 
       <Clubs />
 
       <div className={styles.topScores1}>
         <div className={styles.top}>Topscorers seizoen {season}</div>
-          <div className={styles.scorers}>
+        <div className={styles.scorers}>
           {topScorers &&
             topScorers.slice(0, 3).map((scorer, index) => (
               <div className={styles.topScorers} key={index}>
-                
                 <div className={styles.scorerInfo}>
-                  <img src={scorer.team.crest} alt="" style={{width:"30px"}}/>
-                  <div className={styles.name}>{scorer.player.name} - {scorer.team.shortName}</div>
+                  <Image
+                    src={scorer.team.crest}
+                    width={100}
+                    height={0}
+                    alt=""
+                    style={{ width: "30px", height: "30px" }}
+                  />
+                  <div className={styles.name}>
+                    {scorer.player.name} - {scorer.team.shortName}
+                  </div>
                 </div>
                 <div className={styles.goals}>{scorer.goals} goals</div>
               </div>
             ))}
         </div>
       </div>
-
-      
+      <ScrollToTop />
     </div>
   );
 };
